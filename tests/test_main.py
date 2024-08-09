96966d61e82b3301aaa4e1376480d7c22d9db868
jose <jpgmahedero@gmail.com>
2024-08-09 11:16:48 +0200

    - main.py   - switched from old @app.on_event('startup') to  @asynccontextmanager and  lifespan(app: FastAPI)   - added yield into lifespan function which separates startup from teardown - db.py   - added db as module variable   - added intialize db as a way of populating db with fake data - test:_main.py   - added testing with fastapi.testclient.TestClient   - added simple index test - utils.py   - added call to get_db

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import  pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_index():
+    response = client.get('/')
+
+    assert  response.status_code == 200
+
+
+
