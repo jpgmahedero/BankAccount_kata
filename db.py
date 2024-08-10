a52d297905de591b862d10a352de822534055e12
jose <jpgmahedero@gmail.com>
2024-08-10 12:26:24 +0200

    - db.py   - added async def db_deposit(request: DepositRequest):
    - main.py
      - moved deposit functionallity to db.db_deposit

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 # db.py
 
 from typing import Dict, List, Any
+from fastapi.params import Depends
+from schemas import DepositRequest, DepositResponse
+
 
 global db
 db: Dict[str, List[Any]] = {}
@@ -16,3 +19,15 @@ async def initialize_db() -> None:
 
 async def get_db() -> Dict[str, List[Any]]:
     return db
+
+async def db_deposit(request: DepositRequest):
+    db = await get_db()
+    # Check if the account exists
+    account = next((acc for acc in db["accounts"] if acc["number"] == request.account), None)
+    if not account:
+        raise HTTPException(status_code=404, detail="Account does not exist")
+
+    # Update the balance
+    account["balance"] += request.amount
+
+    return account
